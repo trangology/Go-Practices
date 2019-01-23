2299848d7fb8794269c30d306b6d2284c7354940
Thuy Trang <37827647+nttrang99@users.noreply.github.com>
2019-01-23 19:20:16 +0300

    ok

--- homework03/game_test.go
+++ homework03/game_test.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"io/ioutil"
+	"strings"
+
 	"testing"
 )
+
+var steps [][]string
+var grid [][]Cell
+
 
 func check(e error)  {
 	if e != nil{
@@ -12,8 +18,79 @@ func check(e error)  {
 	}
 }
 
-func TestCell_GetNeighbours(t *testing.T) {
+
+func MakeGridFromFile() {
 	g, err := ioutil.ReadFile("./grid.txt")
 	check(err)
-	fmt.Print(len(g))
+	index := 0
+	grid = make([][]Cell, 6)
+	for i := 0; i < 6; i++ {
+		grid[i] = make([]Cell, 8)
+	}
+
+	for i := 0; i < 6; i++ {
+		for j := 0; j < 8; j++{
+			state := int(g[index]) - 48
+			cell := makeCell(i, j, state)
+			grid[i][j] = cell
+			index++
+		}
+		index += 2  // has 2 symbol not 0 or 1
+	}
+	return
+}
+
+
+func MakeStepsFromFile(content []string) {
+	index := 0
+	for i := 0; i < 2; i++{
+		var step []string
+		for j := 0; j < 6; j++{
+			step = append(step, content[index])
+			index++
+		}
+		// increase index to avoid string = "\r" in "content" variable
+		index++
+		steps = append(steps, step)
+	}
+	return
+}
+
+
+func ReadAllFile() {
+	MakeGridFromFile()
+	f, _ := ioutil.ReadFile("./steps.txt")
+	content := strings.Split(string(f), "\n")
+	MakeStepsFromFile(content)
+	return
+}
+
+func AsserEq(cl [][]Cell, step int) bool {
+	for i := 0; i < 6; i++ {
+		for j := 0; j < 8; j++{
+			for pos, chr := range steps[step][i]{
+				if pos == j {
+					if cl[i][j].state && chr - 48 == 0{
+						return false
+					}
+					if !cl[i][j].state && chr - 48 == 1{
+						return false
+					}
+				}
+			}
+		}
+	}
+	return true
+}
+
+
+func TestCell_GetNeighbours(t *testing.T) {
+	ReadAllFile()
+	cellList := CellList{grid, 8, 6}
+	for step := 0; step < len(steps); step++{
+		newClist := cellList.Update()
+		if !AsserEq(newClist, step){
+			t.Fatalf("Got incorrect result when running at step %d", step + 1)
+		}
+	}
 }
