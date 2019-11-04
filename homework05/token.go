6333ebb3c914669876a191c11d71b6c0d8a80944
trangology <trangology@gmail.com>
2019-11-04 15:58:37 +0300

    fixed bugs and refactored code

--- homework05/token.go
+++ homework05/token.go
@@ -2,5 +2,4 @@ package main
 
 const teleToken string = "681470159:AAG3MA3HVJYYPRjduqsXh8BgBzXp-wiKQDY"
 
-const domain  = "http://www.ifmo.ru/ru/schedule/0/"
-
+const domain = "http://www.ifmo.ru/ru/schedule/0/"
