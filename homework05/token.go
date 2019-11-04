2d7d668c70be032a502ae5774532bd1fdf0b297d
nttrang99 <thuytrang.9906@gmail.com>
2019-11-04 16:02:23 +0300

    update

--- homework05/token.go
+++ homework05/token.go
@@ -1,5 +1,6 @@
 package main
 
-const teleToken string = "681470159:AAG3MA3HVJYYPRjduqsXh8BgBzXp-wiKQDY"
+// ADD YOUR TELEGRAM TOKEN BELOW
+const teleToken string = ""
 
 const domain = "http://www.ifmo.ru/ru/schedule/0/"
