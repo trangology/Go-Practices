ad7af84ef5a9c380c064e715ab6b154094133533
nttrang99 <thuytrang.9906@gmail.com>
2019-11-10 13:41:27 +0300

    a small change

--- homework04/config.go
+++ homework04/config.go
@@ -1,17 +1,17 @@
 package main
 
-const clientId string = "6756360"
+const clientID string = ""
 const scope string = "friends,messages"
 
+// Add your Access token and the version you want below
 var VkConfig = map[string]string{
 	"domain":      "https://api.vk.com/method/",
-	"accessToken": `access_token=3d26d89b5695330509d5a5f7612c7d59ecd3e3519acdf6e6d86c14e1146a9735064495567bd1167cc0b47`,
-	"version":     "&v=5.92",
+	"accessToken": ``,
+	"version":     "&v=",
 }
 
-/**
+// Add your User name and API key below
 var PlotlyConfig = map[string]string{
-	"username": "NT.Trang",
-	"api_key": "9M1PLqRELFrysZLZc0JG",
+	"username": "",
+	"api_key":  "",
 }
-**/
